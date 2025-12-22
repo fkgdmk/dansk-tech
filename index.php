621d1676e550f086db66a47b717038f3941c7b4a
Bo Møller <bm@wmh.dk>
2025-12-22 20:32:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -738,9 +738,9 @@
   <div class="bg-white py-24 sm:py-32 dark:bg-gray-900">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl text-center">
-        <h2 class="text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Del budskabet eller foreslå software</h2>
+        <h2 class="text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Del budskabet</h2>
         <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-400">
-          Hjælp os med at sprede budskabet om den danske tech stack. Del dette med andre danske software-virksomheder, der leder efter alternativer. Har du et forslag til et dansk software-produkt, som vi mangler, så lad os vide det.
+          Hjælp os med at sprede budskabet om den danske tech stack. Del dette med andre danske software-virksomheder, der leder efter alternativer. 
         </p>
         <div class="mt-10 flex items-center justify-center gap-x-4">
           <a href="https://twitter.com/intent/tweet?text=Tjek%20lige%20den%20danske%20tech%20stack%20ud.%20Man%20kan%20sagtens%20k%C3%B8be%20fed%20software%20i%20Danmark.%20Se%20dansktechstack.dk&url=https://dansktechstack.dk" target="_blank" rel="noopener noreferrer" class="rounded-md bg-gray-900 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 dark:bg-white dark:text-gray-900 dark:hover:bg-gray-100">
@@ -752,9 +752,7 @@
           <a href="https://www.facebook.com/sharer/sharer.php?u=https://dansktechstack.dk" target="_blank" rel="noopener noreferrer" class="rounded-md bg-blue-500 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">
             Del på Facebook
           </a>
-          <a href="mailto:kontakt@langsom.com?subject=Forslag%20til%20dansk%20tech%20stack%20-%20dansktechstack.dk" class="rounded-md bg-green-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
-            Send et forslag
-          </a>
+
         </div>
       </div>
     </div>
